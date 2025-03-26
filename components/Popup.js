090a4573a5263e2670bbd498a4525e418370c7f4
Victor E. Morales Perez <vp5662021@gmail.com>
2025-03-25 19:55:56 -0700

    Project 8 : 5

---
+++ components/Popup.js
@@ -0,0 +1,23 @@
+class Popup {
+  constructor({ popupSelector }) {
+    this._popupElement = document.querySelector(popupSelector);
+    this._popupCloseBtn = this._popupElement.querySelector(".popup__close");
+  }
+
+  open() {
+    this._popupElement.classList.add("popup_visible");
+    // document.addEventListener("keydown", handleEscClose);
+  }
+
+  close() {
+    this._popupElement.classList.remove("popup_visible");
+    // document.removeEventListener("keydown", handleEscClose);
+  }
+
+  setEventListeners() {
+    this._popupCloseBtn.addEventListener("click", () => {
+      this.close();
+    });
+  }
+}
+export default Popup;
